0c9a00b4fb0280205522657a4ac163087811801b
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2023-09-24 00:44:15 -0400

    Add files via upload

---
+++ RandomButton/RandomButton.cpp
@@ -0,0 +1,243 @@
+#define YYSDK_PLUGIN
+#include "RandomButton.hpp"	// Include our header
+#include "ModInfo.h"
+#include <Windows.h>    // Include Windows's mess.
+#include <vector>       // Include the STL vector.
+#include <unordered_map>
+#include <functional>
+
+static struct Version {
+	int major = VERSION_MAJOR;
+	int minor = VERSION_MINOR;
+	int build = VERSION_BUILD;
+} version;
+
+static struct Mod {
+	Version version;
+	const char* name = MOD_NAME;
+} mod;
+
+inline void CallOriginal(YYTKCodeEvent* pCodeEvent, CInstance* Self, CInstance* Other, CCode* Code, RValue* Res, int Flags) {
+	if (!pCodeEvent->CalledOriginal()) {
+		pCodeEvent->Call(Self, Other, Code, Res, Flags);
+	}
+}
+
+std::string GetFileName(const char* File) {
+	std::string sFileName(File);
+	size_t LastSlashPos = sFileName.find_last_of("\\");
+	if (LastSlashPos != std::string::npos && LastSlashPos != sFileName.length()) {
+		sFileName = sFileName.substr(LastSlashPos + 1);
+	}
+	return sFileName;
+}
+
+// CallBuiltIn is way too slow to use per frame. Need to investigate if there's a better way to call in built functions.
+
+// We save the CodeCallbackHandler attributes here, so we can unregister the callback in the unload routine.
+static CallbackAttributes_t* g_pFrameCallbackAttributes = nullptr;
+static CallbackAttributes_t* g_pCodeCallbackAttributes = nullptr;
+static uint32_t FrameNumber = 0;
+
+static std::unordered_map<int, const char*> codeIndexToName;
+static std::unordered_map<int, std::function<void(YYTKCodeEvent* pCodeEvent, CInstance* Self, CInstance* Other, CCode* Code, RValue* Res, int Flags)>> codeFuncTable;
+
+static const char* playStr = "Play Modded!";
+RefString tempVar = RefString(playStr, strlen(playStr), false);
+static bool versionTextChanged = false;
+
+// This callback is registered on EVT_PRESENT and EVT_ENDSCENE, so it gets called every frame on DX9 / DX11 games.
+YYTKStatus FrameCallback(YYTKEventBase* pEvent, void* OptionalArgument) {
+	FrameNumber++;
+	// Tell the core the handler was successful.
+	return YYTK_OK;
+}
+
+// This callback is registered on EVT_CODE_EXECUTE, so it gets called every game function call.
+YYTKStatus CodeCallback(YYTKEventBase* pEvent, void* OptionalArgument) {
+	YYTKCodeEvent* pCodeEvent = dynamic_cast<decltype(pCodeEvent)>(pEvent);
+
+	std::tuple<CInstance*, CInstance*, CCode*, RValue*, int> args = pCodeEvent->Arguments();
+
+	CInstance* Self = std::get<0>(args);
+	CInstance* Other = std::get<1>(args);
+	CCode* Code = std::get<2>(args);
+	RValue* Res = std::get<3>(args);
+	int			Flags = std::get<4>(args);
+
+	if (!Code->i_pName) {
+		return YYTK_INVALIDARG;
+	}
+
+	if (codeFuncTable.count(Code->i_CodeIndex) != 0) {
+		codeFuncTable[Code->i_CodeIndex](pCodeEvent, Self, Other, Code, Res, Flags);
+	} else // Haven't cached the function in the table yet. Run the if statements and assign the function to the code index
+	{
+		codeIndexToName[Code->i_CodeIndex] = Code->i_pName;
+		if (_strcmpi(Code->i_pName, "gml_Object_obj_TitleScreen_Create_0") == 0) {
+			auto TitleScreen_Create_0 = [](YYTKCodeEvent* pCodeEvent, CInstance* Self, CInstance* Other, CCode* Code, RValue* Res, int Flags) {
+				if (versionTextChanged == false) {
+					YYRValue yyrv_version;
+					CallBuiltin(yyrv_version, "variable_global_get", Self, Other, { "version" });
+					PrintMessage(CLR_AQUA, "[%s:%d] variable_global_get : version", GetFileName(__FILE__).c_str(), __LINE__);
+					if (yyrv_version.operator std::string().find("Modded") == std::string::npos) {
+						std::string moddedVerStr = yyrv_version.operator std::string() + " (Modded)";
+						CallBuiltin(yyrv_version, "variable_global_set", Self, Other, { "version", moddedVerStr.c_str() });
+						PrintMessage(CLR_TANGERINE, "[%s:%d] variable_global_set : version", GetFileName(__FILE__).c_str(), __LINE__);
+					}
+					versionTextChanged = true;
+				}
+
+				CallOriginal(pCodeEvent, Self, Other, Code, Res, Flags);
+			};
+			TitleScreen_Create_0(pCodeEvent, Self, Other, Code, Res, Flags);
+			codeFuncTable[Code->i_CodeIndex] = TitleScreen_Create_0;
+		}
+		else if (_strcmpi(Code->i_pName, "gml_Object_obj_TextController_Create_0") == 0) {
+			auto TextController_Create_0 = [](YYTKCodeEvent* pCodeEvent, CInstance* Self, CInstance* Other, CCode* Code, RValue* Res, int Flags) {
+				CallOriginal(pCodeEvent, Self, Other, Code, Res, Flags);
+				YYRValue yyrv_textContainer;
+				CallBuiltin(yyrv_textContainer, "variable_global_get", Self, Other, { "TextContainer" });
+				PrintMessage(CLR_AQUA, "[%s:%d] variable_global_get : TextContainer", GetFileName(__FILE__).c_str(), __LINE__);
+				YYRValue yyrv_titleButtons;
+				CallBuiltin(yyrv_titleButtons, "struct_get", Self, Other, { yyrv_textContainer, "titleButtons" });
+				PrintMessage(CLR_AQUA, "[%s:%d] struct_get : titleButtons", GetFileName(__FILE__).c_str(), __LINE__);
+				YYRValue yyrv_eng;
+				CallBuiltin(yyrv_eng, "struct_get", Self, Other, { yyrv_titleButtons, "eng" });
+				PrintMessage(CLR_AQUA, "[%s:%d] struct_get : eng", GetFileName(__FILE__).c_str(), __LINE__);
+				if (std::string(yyrv_eng.RefArray->m_Array[0].String->Get()).find("Modded") == std::string::npos) {
+					yyrv_eng.RefArray->m_Array[0].String = &tempVar;
+					PrintMessage(CLR_TANGERINE, "[%s:%d] variable_global_set : eng[0]", GetFileName(__FILE__).c_str(), __LINE__);
+				}
+			};
+			TextController_Create_0(pCodeEvent, Self, Other, Code, Res, Flags);
+			codeFuncTable[Code->i_CodeIndex] = TextController_Create_0;
+		}
+		else if (_strcmpi(Code->i_pName, "gml_Object_obj_CharSelect_Create_0") == 0) {
+			auto CharSelect_Create_0 = [](YYTKCodeEvent* pCodeEvent, CInstance* Self, CInstance* Other, CCode* Code, RValue* Res, int Flags) {
+				CallOriginal(pCodeEvent, Self, Other, Code, Res, Flags);
+
+				YYRValue yyrv_characterData;
+				CallBuiltin(yyrv_characterData, "variable_global_get", Self, Other, { "characterData" });
+				YYRValue yyrv_random;
+				CallBuiltin(yyrv_random, "ds_map_find_value", Self, Other, { yyrv_characterData, "random" });
+
+				YYRValue yyrv_charListByGen;
+				CallBuiltin(yyrv_charListByGen, "variable_instance_get", Self, Other, { (long long)Self->i_id, "charListByGen" });
+
+				YYRValue yyrv_lastGenIndex;
+				yyrv_lastGenIndex.Kind = VALUE_REAL;
+				yyrv_lastGenIndex.Real = yyrv_charListByGen.RefArray->length - 1;
+				YYRValue yyrv_lastGenCharList;
+				CallBuiltin(yyrv_lastGenCharList, "array_get", Self, Other, { yyrv_charListByGen, yyrv_lastGenIndex });
+
+				YYRValue yyrv_result;
+				CallBuiltin(yyrv_result, "array_push", Self, Other, { yyrv_lastGenCharList, yyrv_random });
+
+				YYRValue yyrv_lastCharIndex;
+				yyrv_lastCharIndex.Kind = VALUE_REAL;
+				yyrv_lastCharIndex.Real = yyrv_lastGenCharList.RefArray->length - 1;
+				YYRValue yyrv_lastGenChar;
+				CallBuiltin(yyrv_lastGenChar, "array_get", Self, Other, { yyrv_lastGenCharList, yyrv_lastCharIndex });
+
+				YYRValue yyrv_id;
+				CallBuiltin(yyrv_id, "struct_get", Self, Other, { yyrv_lastGenChar, "id" });
+				PrintMessage(CLR_BRIGHTPURPLE, "yyrv_id = %s", yyrv_id.String->Get()); 
+			};
+			CharSelect_Create_0(pCodeEvent, Self, Other, Code, Res, Flags);
+			codeFuncTable[Code->i_CodeIndex] = CharSelect_Create_0;
+		}
+		else
+		{
+			auto UnmodifiedFunc = [](YYTKCodeEvent* pCodeEvent, CInstance* Self, CInstance* Other, CCode* Code, RValue* Res, int Flags) {
+				CallOriginal(pCodeEvent, Self, Other, Code, Res, Flags);
+			};
+			UnmodifiedFunc(pCodeEvent, Self, Other, Code, Res, Flags);
+			codeFuncTable[Code->i_CodeIndex] = UnmodifiedFunc;
+		}
+	}
+	// Tell the core the handler was successful.
+	return YYTK_OK;
+}
+
+// Create an entry routine - it must be named exactly this, and must accept these exact arguments.
+// It must also be declared DllExport (notice how the other functions are not).
+DllExport YYTKStatus PluginEntry(YYTKPlugin* PluginObject) {
+	// Set the unload routine
+	PluginObject->PluginUnload = PluginUnload;
+
+	// Print a message to the console
+	PrintMessage(CLR_DEFAULT, "[%s v%d.%d.%d] - Hello from PluginEntry!", mod.name, mod.version.major, mod.version.minor, mod.version.build);
+
+	PluginAttributes_t* PluginAttributes = nullptr;
+
+	// Get the attributes for the plugin - this is an opaque structure, as it may change without any warning.
+	// If Status == YYTK_OK (0), then PluginAttributes is guaranteed to be valid (non-null).
+	if (YYTKStatus Status = PmGetPluginAttributes(PluginObject, PluginAttributes)) {
+		PrintError(__FILE__, __LINE__, "[%s v%d.%d.%d] - PmGetPluginAttributes failed with 0x%x", mod.name, mod.version.major, mod.version.minor, mod.version.build, Status);
+		return YYTK_FAIL;
+	}
+
+	// Register a callback for frame events
+	YYTKStatus Status = PmCreateCallback(
+		PluginAttributes,					// Plugin Attributes
+		g_pFrameCallbackAttributes,				// (out) Callback Attributes
+		FrameCallback,						// The function to register as a callback
+		static_cast<EventType>(EVT_PRESENT | EVT_ENDSCENE), // Which events trigger this callback
+		nullptr								// The optional argument to pass to the function
+	);
+
+	if (Status) {
+		PrintError(__FILE__, __LINE__, "[%s v%d.%d.%d] - PmCreateCallback failed with 0x%x", mod.name, mod.version.major, mod.version.minor, mod.version.build, Status);
+		return YYTK_FAIL;
+	}
+
+	// Register a callback for frame events
+	Status = PmCreateCallback(
+		PluginAttributes,					// Plugin Attributes
+		g_pCodeCallbackAttributes,			// (out) Callback Attributes
+		CodeCallback,						// The function to register as a callback
+		static_cast<EventType>(EVT_CODE_EXECUTE), // Which events trigger this callback
+		nullptr								// The optional argument to pass to the function
+	);
+
+	if (Status) {
+		PrintError(__FILE__, __LINE__, "[%s v%d.%d.%d] - PmCreateCallback failed with 0x%x", mod.name, mod.version.major, mod.version.minor, mod.version.build, Status);
+		return YYTK_FAIL;
+	}
+
+	// Off it goes to the core.
+	return YYTK_OK;
+}
+
+// The routine that gets called on plugin unload.
+// Registered in PluginEntry - you should use this to release resources.
+YYTKStatus PluginUnload() {
+	YYTKStatus Removal = PmRemoveCallback(g_pFrameCallbackAttributes);
+
+	// If we didn't succeed in removing the callback.
+	if (Removal != YYTK_OK) {
+		PrintError(__FILE__, __LINE__, "[%s v%d.%d.%d] PmRemoveCallback failed with 0x%x", mod.name, mod.version.major, mod.version.minor, mod.version.build, Removal);
+	}
+
+	Removal = PmRemoveCallback(g_pCodeCallbackAttributes);
+
+	// If we didn't succeed in removing the callback.
+	if (Removal != YYTK_OK) {
+		PrintError(__FILE__, __LINE__, "[%s v%d.%d.%d] PmRemoveCallback failed with 0x%x", mod.name, mod.version.major, mod.version.minor, mod.version.build, Removal);
+	}
+
+	PrintMessage(CLR_DEFAULT, "[%s v%d.%d.%d] - Goodbye!", mod.name, mod.version.major, mod.version.minor, mod.version.build);
+
+	return YYTK_OK;
+}
+
+// Boilerplate setup for a Windows DLL, can just return TRUE.
+// This has to be here or else you get linker errors (unless you disable the main method)
+BOOL APIENTRY DllMain(
+	HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+) {
+	return 1;
+}
